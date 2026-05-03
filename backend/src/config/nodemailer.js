2e400af37253f8b1e7e24904ece59857b189eaba
Mehedi54357 <mdmehedihasanmahfuz1@gmail.com>
2026-05-03 21:48:55 +0600

    fix: default to smtp.gmail.com if EMAIL_HOST is undefined

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -1,8 +1,8 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  host: process.env.EMAIL_HOST || 'smtp.gmail.com',
+  port: process.env.EMAIL_PORT || 587,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
